1e76a34dd73bd7c69a491f29200c8b1e29ea0ef0
vigalaksa0906 <vigalucubanget@gmail.com>
2024-04-26 14:09:55 +0700

    add: upload files

---
+++ main.py
@@ -0,0 +1,13 @@
+name = 'Viga'
+age = 23
+address = 'Purworejo, Central Java'
+
+# Hello, my name is Viga, I'm 23 years old and I'm from Purworejo, Central Java
+# print biasa
+print("Hello, my name is", name + ", I'm",age,"years old and I'm from", address)
+
+# f string 
+print(f"Hello, my name is {name}, I'm {age} years old, and I'm from {address}")
+
+# format
+print("Hello, my name is {}, I'm {} years old and I'm from {}".format(name, age, address))
